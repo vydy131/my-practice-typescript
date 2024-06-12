69b3b5fa2943334a355d6f0eb89b0399bb882a3d
vydy131 <vydy131@gmail.com>
2024-06-12 10:52:08 +0300

    make first object

--- hello.ts
+++ hello.ts
@@ -1,6 +1,17 @@
-let array:number[] = [1,2,3,4]
-let arr:Array<number> = [1,2,3,4]
-let result : boolean = true
+let object = {year : 2024, month : 'june', day : 12}
+console.log(object.month)
+
+
+
+
+
+
+
+
+
+// let array:number[] = [1,2,3,4]
+// let arr:Array<number> = [1,2,3,4]
+// let result : boolean = true
 // if (arr != array)       // there is a mistake
 //     result = false
-console.log(result)
+// console.log(result)
