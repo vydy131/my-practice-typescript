cf155714a0ef957f89ee8054a7a7a04b55ccd86f
vydy131 <vydy131@gmail.com>
2024-06-12 21:46:02 +0300

    type directive

--- hello.ts
+++ hello.ts
@@ -9,3 +9,14 @@ function sum(array : number[]):number{
 
 let array : number[] = [1,3,4,5]
 console.log(array, ' = ', sum(array))
+
+let nn : number | null;
+let nsb : number | string | boolean;
+let aaa : any
+let vvv : void
+
+type nu = null | undefined
+type bnu = boolean | nu
+
+type stat_type = 'error' | 'warning' | 'success'
+let prog_stat : stat_type = 'warning'
