469e3251aff1882300d93168574fec4ae942e199
vydy131 <vydy131@gmail.com>
2024-06-12 23:08:58 +0300

    array in array and ...spread

--- hello.ts
+++ hello.ts
@@ -1,23 +1,38 @@
-let user: [string, number, boolean?];
+// let user: [string, number, boolean?];
 
-user = ['vova', 31]
-console.log(user)
+// user = ['vova', 31]
+// console.log(user)
 
-user[2] = false
-console.log(user)
+// user[2] = false
+// console.log(user)
 
-let user_r: readonly [string, number]
+// let user_r: readonly [string, number]
 
-let array_a : any[] = ['vova', 31, 'qwerty', false, true, undefined]
-let array_ns : (number | string)[] = ['vova', 31, 'qwerty', 42]
+// let array_a : any[] = ['vova', 31, 'qwerty', false, true, undefined]
+// let array_ns : (number | string)[] = ['vova', 31, 'qwerty', 42]
 
-console.log(array_a)
-console.log(array_ns)
+// console.log(array_a)
+// console.log(array_ns)
 
-interface my_inter {
-    [key : string] : number | undefined | string | boolean
-}
+// interface my_inter {
+//     [key : string] : number | undefined | string | boolean
+// }
 
-let time: [number, number, number] = [12, 59, 59];
-let [hour, minute, second] = time
-console.log(hour, minute, second)
+// let time: [number, number, number] = [12, 59, 59];
+// let [hour, minute, second] = time
+// console.log(hour, minute, second)
+
+let arr1: number[] = [1,2,3,4]
+let arr2: number[] = [...arr1,1,2,3,4]
+let arr3: (number|number[])[] = [arr1,1,2,3,4]
+
+/*[ 1, 2, 3, 4 ]
+[
+  1, 2, 3, 4,
+  1, 2, 3, 4
+]
+[ [ 1, 2, 3, 4 ], 1, 2, 3, 4 ]*/
+
+console.log(arr1)
+console.log(arr2)
+console.log(arr3)
