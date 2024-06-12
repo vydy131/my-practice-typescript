c1202d5fe8258352d6fa7c785fbf4e4195ce29f8
vydy131 <vydy131@gmail.com>
2024-06-12 21:01:33 +0300

    my first function

--- hello.ts
+++ hello.ts
@@ -1,20 +1,5 @@
-interface MyObject {
-    [key: string]:number | undefined | boolean;
-    a: number;
-    b: number;
-    c: number;
-    x?: number;
-    z?: boolean;
+function sum(arg1:number, arg2:number) : number{
+    return arg2+arg1;
 }
 
-let obj: MyObject = {a: 1, b:2, z:false, d:4, c: 3, "123":123};
-let res: number = 0;
-
-for (let key in obj) {
-    let elem = obj[key];
-    if (elem !== undefined && typeof elem !== 'boolean'){
-        res += elem;}
-    console.log(key, ":\t", obj[key])
-}
-
-console.log("Result is ", res);
+console.log(sum(1,2))
