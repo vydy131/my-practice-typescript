bd7b8682f71daa1affb53dcd1952238460684cd0
vydy131 <vydy131@gmail.com>
2024-06-13 17:30:01 +0300

    function type

--- hello.ts
+++ hello.ts
@@ -1,38 +1,20 @@
-// let user: [string, number, boolean?];
+// enum Week {monday, tuesday, wednesday, thursday, friday, saturday, sunday}
+// let day : string = Week[0]
+// console.log(day)
 
-// user = ['vova', 31]
-// console.log(user)
+// let key : number = Week.sunday
+// console.log(key)
 
-// user[2] = false
-// console.log(user)
+// let now : Date = new Date
+// let whenever : Date = new Date(2021, 10, 3)
+// console.log(now, "\t", whenever)
 
-// let user_r: readonly [string, number]
 
-// let array_a : any[] = ['vova', 31, 'qwerty', false, true, undefined]
-// let array_ns : (number | string)[] = ['vova', 31, 'qwerty', 42]
+// let fun_var : ( x: number, y: number) => number;
+// function sum (a: number, b: number): number {return a+b}
+// fun_var = sum
 
-// console.log(array_a)
-// console.log(array_ns)
-
-// interface my_inter {
-//     [key : string] : number | undefined | string | boolean
-// }
-
-// let time: [number, number, number] = [12, 59, 59];
-// let [hour, minute, second] = time
-// console.log(hour, minute, second)
-
-let arr1: number[] = [1,2,3,4]
-let arr2: number[] = [...arr1,1,2,3,4]
-let arr3: (number|number[])[] = [arr1,1,2,3,4]
-
-/*[ 1, 2, 3, 4 ]
-[
-  1, 2, 3, 4,
-  1, 2, 3, 4
-]
-[ [ 1, 2, 3, 4 ], 1, 2, 3, 4 ]*/
-
-console.log(arr1)
-console.log(arr2)
-console.log(arr3)
+// let func : (text:string) => void
+// func = function(text: string): void {
+// 	alert(text);
+// };
