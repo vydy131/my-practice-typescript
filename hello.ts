8863a1e5fb6e0be9d820319be39120cce1cba622
vydy131 <vydy131@gmail.com>
2024-06-13 20:28:42 +0300

    classes and oop

--- hello.ts
+++ hello.ts
@@ -1,6 +1,7 @@
 class Man {
     _name : string
     _age : number
+    static _quantity : number = 0
 
     age(numb ?: number) : number {
         if (numb !== undefined){
@@ -19,5 +20,26 @@ class Man {
     constructor(name: string = '', age: number = 0){
         this._name = name;
         this._age = age;
+        Man._quantity++
     }
 }
+
+class Pupil extends Man {
+    _school : string
+
+    constructor(name: string = '', age: number = 0, school : string = ''){
+        super(name, age)
+        this._school = school
+    }
+
+    school(str ?: string) : string {
+        if (str !== undefined){
+            this._school = str
+        }
+        return this._school;
+    }
+
+}
+
+const someone : Man = new Man('Somebody', 10)
+console.log(someone)
