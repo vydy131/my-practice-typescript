ccaabad30fd119df276c0f1ffe8e960641a53423
vydy131 <vydy131@gmail.com>
2024-06-14 16:47:54 +0300

    interfaces

--- hello.ts
+++ hello.ts
@@ -1,45 +1,26 @@
-class Man {
-    _name : string
-    _age : number
-    static _quantity : number = 0
+interface Circle{
+    kind : 'circle'
+    radius : number
+}
 
-    age(numb ?: number) : number {
-        if (numb !== undefined){
-            this._age = numb
-        }
-        return this._age;
+interface Rectangle {
+    kind : 'rectangle'
+    length : number
+    width : number
+}
+
+type Figure = Circle | Rectangle
+
+function area(fig : Figure) : number {
+    if (fig.kind === "circle"){
+        return (fig.radius ** 2) * 3.14
     }
-
-    name(str ?: string) : string {
-        if (str !== undefined){
-            this._name = str
-        }
-        return this._name;
-    }
-
-    constructor(name: string = '', age: number = 0){
-        this._name = name;
-        this._age = age;
-        Man._quantity++
+    else {
+        return fig.length * fig.width
     }
 }
 
-class Pupil extends Man {
-    _school : string
-
-    constructor(name: string = '', age: number = 0, school : string = ''){
-        super(name, age)
-        this._school = school
-    }
-
-    school(str ?: string) : string {
-        if (str !== undefined){
-            this._school = str
-        }
-        return this._school;
-    }
-
-}
-
-const someone : Man = new Man('Somebody', 10)
-console.log(someone)
+let fig : Figure = {kind : "circle", radius : 5}
+console.log(area(fig))
+fig = {kind : 'rectangle', length : 10, width : 5}
+console.log(area(fig))
